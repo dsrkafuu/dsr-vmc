270e9227d48da56355a3fbf489c62d7241f4e8b6
dsrkafuu <dsrkafuu@outlook.com>
2022-06-15 17:29:48 +0800

    init: dsrvmc package v19.0.0

---
+++ README.md
@@ -0,0 +1,75 @@
+# DSRVMC Vanilla Modpack
+
+Lightweight vanilla modpack for DSRVMC project.
+
+For players from China, check out [DSRTOOLS](https://tools.dsrkafuu.net/minecraft) for more details.
+
+## Details
+
+Starts from Minecraft 1.4.7 in 2012, the modpack is for those who want to join vanilla servers without too much QoL features.
+
+Performace improvement is the main goal.
+
+It's also great for using as a base for your own large modpack.
+
+## Build
+
+Dependencies:
+
+- packwiz: https://packwiz.infra.link/
+- Node.js: https://nodejs.org/
+
+Add more mods:
+
+```bash
+packwiz modrinth install
+packwiz curseforge install
+```
+
+Build the package:
+
+```bash
+npm install
+npm run build
+```
+
+## References
+
+### Mods
+
+- AppleSkin: https://modrinth.com/mod/appleskin
+- Architectury API: https://modrinth.com/mod/architectury-api
+- bad packets: https://modrinth.com/mod/badpackets
+- Cloth Config API: https://modrinth.com/mod/cloth-config
+- Controlling: https://www.curseforge.com/minecraft/mc-mods/controlling
+- CustomSkinLoader: https://www.curseforge.com/minecraft/mc-mods/customskinloader
+- Durability Viewer: https://modrinth.com/mod/durabilityviewer
+- Dynamic FPS: https://modrinth.com/mod/dynamic-fps
+- Fabric API: https://modrinth.com/mod/fabric-api
+- FerriteCore: https://modrinth.com/mod/ferrite-core
+- Indium: https://modrinth.com/mod/indium
+- Inventory Profiles Next: https://modrinth.com/mod/inventory-profiles-next
+- Iris Shaders: https://modrinth.com/mod/iris
+- Krypton: https://modrinth.com/mod/krypton
+- LazyDFU: https://modrinth.com/mod/lazydfu
+- Lithium: https://modrinth.com/mod/lithium
+- Memory Leak Fix: https://modrinth.com/mod/memoryleakfix
+- Mod Menu: https://modrinth.com/mod/modmenu
+- Not Enough Crashes: https://modrinth.com/mod/notenoughcrashes
+- Roughly Enough Items Fabric/Forge (REI): https://www.curseforge.com/minecraft/mc-mods/roughly-enough-items
+- ShulkerBoxTooltip: https://www.curseforge.com/minecraft/mc-mods/shulkerboxtooltip
+- Sodium: https://modrinth.com/mod/sodium
+- Starlight (Fabric): https://modrinth.com/mod/starlight
+- WTHIT: https://modrinth.com/mod/wthit
+- Xaero's Minimap: https://www.curseforge.com/minecraft/mc-mods/xaeros-minimap
+- Xaero's World Map: https://www.curseforge.com/minecraft/mc-mods/xaeros-world-map
+
+### Shaders
+
+- BSL: https://bitslablab.com/bslshaders
+
+## License
+
+Released under `Apache License 2.0`, for more information read the [LICENSE](https://github.com/dsrkafuu/dsr-vmc/blob/main/LICENSE).
+
+**Copyright © 2013-present DSRKafuU (<https://dsrkafuu.net>)**
---
+++ build.mjs
@@ -0,0 +1,91 @@
+// #!/usr/bin/env zx
+/**
+ * build modpack
+ */
+import path from "path";
+import fse from "fs-extra";
+import toml from "toml";
+import chalk from "chalk";
+import AdmZip from "adm-zip";
+import glob from "glob";
+const { version } = fse.readJSONSync("package.json");
+
+// fix for windows $''
+$.quote = (s) => s;
+
+// build modpacks
+console.log(chalk.blue("building modrinth modpack..."));
+await $`packwiz modrinth export`;
+console.log(chalk.blue("building curseforge modpack..."));
+await $`packwiz curseforge export`;
+
+fse.ensureDirSync("dist");
+fse.emptyDirSync("dist");
+
+// extract mod files
+console.log(chalk.blue("extracting mod files..."));
+const mrpack = new AdmZip(`DSRVMC Vanilla Modpack-${version}.mrpack`);
+mrpack.getEntries().forEach((entry) => {
+  if (entry.entryName.endsWith(".jar")) {
+    console.log(chalk.blue(`extracting ${path.basename(entry.entryName)}...`));
+    mrpack.extractEntryTo(entry.entryName, "dist/mods", false, true);
+  }
+});
+const cfpack = new AdmZip(`DSRVMC Vanilla Modpack-${version}.zip`);
+cfpack.getEntries().forEach((entry) => {
+  if (entry.entryName.endsWith(".jar")) {
+    console.log(chalk.blue(`extracting ${path.basename(entry.entryName)}...`));
+    cfpack.extractEntryTo(entry.entryName, "dist/mods", false, true);
+  }
+});
+
+// copy overrides
+console.log(chalk.blue("processing overrides..."));
+fse.copySync("overrides", "dist", { overwrite: true });
+
+// generate json & markdown
+let markdown = "\n";
+const json = {
+  time: Date.now(),
+  release: "",
+  version: "",
+  mods: [],
+  modrinth: "https://modrinth.com/modpack/dsrvmc",
+  curseforge: "https://www.curseforge.com/minecraft/modpacks/dsrvmc",
+  java: "https://pan.baidu.com/s/11a6jx0MNM8BfrgDum1Ku_w?pwd=cksq",
+  package: "https://pan.baidu.com/s/1HwO9hWnQtIFzegXS5WOFyA?pwd=4ii1",
+};
+const pack = toml.parse(fse.readFileSync("pack.toml", "utf-8"));
+json.release = pack.version;
+json.version = pack.versions.minecraft;
+const mods = glob.sync("mods/*.pw.toml");
+mods.forEach((mod) => {
+  const tomlFile = fse.readFileSync(mod, "utf-8");
+  const basename = path.basename(mod, ".pw.toml");
+  const modinfo = toml.parse(tomlFile);
+  const modName = modinfo.name;
+  let modSource = -1;
+  let modLink = "";
+  if (modinfo.update.modrinth) {
+    modSource = 0;
+    modLink = `https://modrinth.com/mod/${basename}`;
+  } else if (modinfo.update.curseforge) {
+    modSource = 1;
+    modLink = `https://www.curseforge.com/minecraft/mc-mods/${basename}`;
+  }
+  markdown += `- ${modName}: ${modLink}\n`;
+  json.mods.push({
+    name: modName,
+    source: modSource,
+    link: modLink,
+  });
+});
+fse.writeJSONSync("index.json", json);
+let mdfile = fse.readFileSync("README.md", "utf-8");
+mdfile = mdfile.replace(
+  /### Mods[^]*### Shaders/,
+  `### Mods\n${markdown}\n### Shaders`
+);
+fse.writeFileSync("README.md", mdfile);
+
+console.log(chalk.green(`done for version ${version}`));
